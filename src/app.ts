d5fa2b321439786ef4b631a02864f81ba6b3cbee
Ahmad-Abulfotouh <ahmad.ahmad171615@gmail.com>
2026-03-27 18:51:05 +0200

    adding agian.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import authRouter from './api/v1/routes/authRoutes.js'
 import userRouter from './api/v1/routes/userRoutes.js'
 import todoRouter from './api/v1/routes/todoRoutes.js'
@@ -6,6 +7,7 @@ import { authenticateToken } from './middlewares/authMiddleware.js';
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to PERN Todo API!" });
